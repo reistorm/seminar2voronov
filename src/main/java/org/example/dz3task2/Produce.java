55eb3041a56d72362846dde82fd602adad4b7262
reistorm <rysaeva96@mail.ru>
2022-12-29 15:17:29 +0300

    задача 2 дз 3(Produce)

---
+++ src/main/java/org/example/dz3task2/Produce.java
@@ -0,0 +1,75 @@
+package org.example.dz3task2;
+
+// Сведения о товаре состоят из наименования,
+// страны-производителя, веса, цены, сорта.
+// Получить наименования товаров заданного сорта с наименьшей ценой.
+public class Produce {
+    private String name;
+    private String country;
+    private Double weight;
+    private Double price;
+    private String grade;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public Double getWeight() {
+        return weight;
+    }
+
+    public void setWeight(Double weight) {
+        this.weight = weight;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+    public String getGrade() {
+        return grade;
+    }
+
+    public void setGrade(String grade) {
+        this.grade = grade;
+    }
+
+    public Produce(String name, String country, Double weight, Double price, String grade) {
+        this.name = name;
+        this.country = country;
+        this.weight = weight;
+        this.price = price;
+        this.grade = grade;
+    }
+
+    @Override
+    public String toString() {
+        return "Produce{" +
+                "name='" + name + '\'' +
+                ", country='" + country + '\'' +
+                ", weight=" + weight +
+                ", price=" + price +
+                ", grade=" + grade +
+                '}';
+    }
+
+
+}
+
+
