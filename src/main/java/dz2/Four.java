d863c4b1291a2cb8fa1cc47d2f199285c0aa73f5
reistorm <rysaeva96@mail.ru>
2022-12-29 21:57:06 +0300

    дз 2 задание 4

--- src/main/java/dz2/Four.java
+++ src/main/java/dz2/Four.java
@@ -7,17 +7,25 @@ import java.util.Scanner;
 //Дана последовательность из N целых чисел.
 //Верно ли, что последовательность является возрастающей.
 public class Four {
-    public static boolean main(String[] args) {
+    public static void main(String[] args) {
         int a, b, n;
         Scanner scanner = new Scanner(System.in);
         n = scanner.nextInt();
         a = scanner.nextInt();
-        for (int i = 0; i < n; i++) {
+        boolean result = false;
+        for (int i = 1; i < n; i++) {
             b = scanner.nextInt();
-            if (a > b)
-                return false;
-            a = b;
+            if (a < b) {
+                result = true;
+                a = b;
+            }
+            else {
+                result = false;
+            }
+
         }
-        return true;
+        scanner.close();
+        System.out.println(result);
     }
+
 }
