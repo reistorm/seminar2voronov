56e74e3fd714281bf9a4886d50cca796c926a79d
reistorm <rysaeva96@mail.ru>
2022-12-29 21:17:36 +0300

    ООП конвертер градусов

---
+++ src/main/java/dz2/Converter/Converter.java
@@ -0,0 +1,36 @@
+package dz2.Converter;
+//Напишите интерфейс Converter для конвертации из градусов по Цельсию в
+//Кельвины, Фаренгейты. У классов наследников необходимо переопределить
+// метод интерфейса, для валидного перевода величин.
+// Метод для конвертации назовите "convertValue".
+
+public class Converter {
+    private Double celsius;
+
+    public Converter(Double celsius) {
+        this.celsius = celsius;
+    }
+
+    public Double getCelsius() {
+        return celsius;
+    }
+
+    @Override
+    public String toString() {
+        return "Converter{" +
+                "Celsius=" + celsius +
+                '}';
+    }
+
+    public void setCelsius(Double celsius) {
+        celsius = celsius;
+    }
+    public double celsiusToFahrenheit() {
+        return celsius * 1.8 + 32;
+    }
+    public double celsiusToKelvin() {
+        return celsius + 273.15;
+    }
+}
+
+
---
+++ src/main/java/dz2/Converter/convertValue.java
@@ -0,0 +1,9 @@
+package dz2.Converter;
+
+public class convertValue {
+    public static void main(String[] args) {
+        Converter converter = new Converter(24.0);
+        System.out.println("Fahrenheit = " + converter.celsiusToFahrenheit());
+        System.out.println("Kelvin = " + converter.celsiusToKelvin());
+    }
+}
