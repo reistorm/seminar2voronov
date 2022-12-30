10adcefdd1501da73f55323e724f90aa15b02953
reistorm <rysaeva96@mail.ru>
2022-12-30 18:40:22 +0300

    дз 2 задание 6

--- src/main/java/dz2/Six.java
+++ src/main/java/dz2/Six.java
@@ -2,8 +2,32 @@ package dz2;
 // Дан массив целых чисел. Заменить отрицательные элементы
 // на сумму индексов двузначных элементов массива.
 
+import java.util.Scanner;
+
 public class Six {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        int[] mass = new int[n];
+        for (int i = 0; i < n; i++) {
+            mass[i] = scanner.nextInt();
+        }
+        int sum = 0;
+        for (int i = 0; i < n; i++) {
 
+            if ((mass[i] < 100) && (mass[i] > 9) || ((mass[i] > -100) && (mass[i] < -9))) {
+                sum += i;
+            }
+        }
+        for (int i = 0; i < n; i++) {
+            if (mass[i] < 0) {
+                mass[i] = sum;
+            }
+            System.out.println(mass[i]);
+        }
+
+
+
+        scanner.close();
     }
 }
